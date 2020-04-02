f1036e6dcf6ae098e0fa556f8e4d23035a1b628c
Zijing Zhu <zzhu17@ucsc.edu>
2020-04-02 16:45:05 -0700

    rount

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/hist')
+def hist():
+  return render_template('hist.html')
+
+@app.route('/price')
+def price():
+  return render_template('close.html')
+
+
 if __name__ == '__main__':
   app.run(port=33507)
